c8585005c49ff967a175a303df366343591fa9fd
Otito-praise <praisecynthia85@yahoo.com>
2022-10-28 17:06:57 +0100

    user commit

---
+++ README.md
@@ -0,0 +1,1 @@
+/loadRegister - registration page
---
+++ middlewares/auth.js
@@ -0,0 +1,27 @@
+const isLoggedIn = async (req, res, next) => {
+  try {
+    if (req.session.userid) {
+    } else {
+      return res.redirect("/login");
+    }
+    next();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// created a middleware
+const isLoggedOut = async (req, res, next) => {
+  try {
+    // we are saying if user has session id they can go to home
+    if (req.session.userid) {
+      return res.redirect("/home");
+    }
+    next();
+  } catch (error) {
+    console.log(error);
+    next();
+  }
+};
+
+module.exports = { isLoggedIn, isLoggedOut };
